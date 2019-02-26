4aad63af94b06bee4f6aa9c64173d13395650dda
Ankit Mittal <ankitml@gmail.com>
2019-02-26 11:42:08 -0500

    first version of setup file

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name='sharedtools',
+    version='0.01',
+    author="SB Ca",
+    author_email="canada@swissborg.com",
+    description="Shared tools for jupyter notebook",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/swissborgcanada/sharedtools",
+    packages=setuptools.find_packages(),
+    classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
