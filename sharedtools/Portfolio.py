1b9b66238725318c5d173a61e58eb701f979b3b9
swissborgcanada <48025383+swissborgcanada@users.noreply.github.com>
2019-03-08 13:04:29 -0500

    Update Portfolio.py

--- sharedtools/Portfolio.py
+++ sharedtools/Portfolio.py
@@ -277,7 +277,7 @@ class Portfolio(object):
             self.statistics['Time Series']['Sharpe Ratio']=SR
             
             if self.benchmark is not None:
-                total_ret_bm=self.benchmark.Benchmark[-1]/self.benchmark.Benchmark[0]-1
+                total_ret_bm=self.benchmark.Benchmark.iloc[-1]/self.benchmark.Benchmark[0]-1
                 IR=(total_ret-total_ret_bm)/std_ann
                 self.statistics['Benchmark']={}
                 self.statistics['Benchmark']['Total Return']=total_ret_bm
