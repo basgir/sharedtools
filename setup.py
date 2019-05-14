7dac50b41b56e36fd1e03f26d694f026764d7d49
swissborgcanada <48025383+swissborgcanada@users.noreply.github.com>
2019-05-14 11:47:14 -0400

    Update setup.py
    print stats

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='sharedtools',
-    version='0.147',
+    version='0.148',
     author="SB Ca",
     author_email="canada@swissborg.com",
     description="Shared tools for jupyter notebook",
